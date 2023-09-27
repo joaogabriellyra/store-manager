01d0eddaeb6a3ddf72e557df4169a05d6c2bc23c
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-09-27 20:29:33 -0300

    fixing everything to show in github

--- src/app.js
+++ src/app.js
@@ -4,16 +4,12 @@ const productsRoutes = require('./routers/products.router');
 const salesRoutes = require('./routers/sales.router');
 
 const app = express();
+
 app.use(bodyParser.json());
-// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-
 app.use('/products', productsRoutes);
 app.use('/sales', salesRoutes);
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo server.js para executar sua aplicação 
 module.exports = app;
